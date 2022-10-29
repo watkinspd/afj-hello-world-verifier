561b7bef42391c8a94ad2eec33b73f8ca2f57d99
Peter Watkins <pdwatkins@me.com>
2022-10-29 13:05:55 -0700

    simplify server.ts

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,6 @@ import {
 } from '@aries-framework/core'
 import { HttpInboundTransport, agentDependencies } from '@aries-framework/node'
 var qrc = require ('qrcode')
-
 const decode = (str: string):string => Buffer.from(str, 'base64').toString('binary')
 
 const serviceEndpoint = 'faber-pdub.ngrok.io'   //needs to match your ngrok session
@@ -24,11 +23,11 @@ const serviceEndpointPort = 8020                // needs to match your ngrok ses
 const walletLabel = 'afj-hellow-world-verifier'
 const walletKey = 'notagreatkey00000000000000000000'
 const listenPort = 3000
-const ledgerID = 'CANdyDev'
 const CANdyDev = `{"reqSignature":{},"txn":{"data":{"data":{"alias":"dev-bc-1","blskey":"14YvF9m4TU4FSkqZQ9gDRr9Ef95aHPuieToEPTmMNEWAqkKB75FfGM3rU25Sfhqkd8nmGMjaJDso9jWgoQdaPqQxscuxu7VcDBmkByiZWtVohk4mRxSownozaaYESpyte4A346wXfGcqxCvUUa9gSyU1qEaM6ss4Z3e5pc39hzdekAq","blskey_pop":"RXmiB6JYFKU6RJwdhfwsH9mGSJx4X8s2Dsk3gHn7Ytajsn6kuaBC6FFiRmzbKkDuYFnMzF81JuRKHC7NBrKgsh8egmyGm5APNya6W6G7XyWwGs2WHX5tUtHeBVogSQEjV8Yq4RfzbpM6TJzPDzu8HyM1Why2nX9f84DMxgxJPMJseo","client_ip":"3.99.10.96","client_port":"9702","node_ip":"3.98.254.147","node_port":"9701","services":["VALIDATOR"]},"dest":"FgRxLSbzVzcMC8ioAbzbzYi1oqhYLUsoFeDewYiw97U"},"metadata":{"from":"7iLf2c7weDopmBLyPPGLHz"},"type":"0"},"txnMetadata":{"seqNo":1,"txnId":"26ac1e1cd83aa136090321bc58877f8563d1ed14cf11e78e1eb46a33692580f1"},"ver":"1"}
 {"reqSignature":{},"txn":{"data":{"data":{"alias":"dev-qc-1","blskey":"NFLKzusQk9RfCAvmw5z56eFXVDhQahAwd8KoxNU4kaikT9RTUBTx537oswApMmTwFhcsrfTpwUm49QLHcVMNY4oZMyoq1ok1jZnUKvkPjqFkYahCr8VefsknaUj6qWiqoqFB1hLCdP3jeXvZbDVQSBnjTSysUkgnVwXEXdmAjt2ovn","blskey_pop":"QtrKM3pBvf9UnNgihnpoC86kVSm1VkLSj6maz9FGupZXcYcsjPaU8fbibUboW6NwzT9WfKpwCTmebDfPC6dieNZpgwxee8WDXHF7nBsgH8ctY2P3aezsEDs2zYSiCvrbdFkxBKvjNMkesK3xKUwGVUMyggjGckVFG2zZgUQ8pY4LKy","client_ip":"3.96.89.59","client_port":"9702","node_ip":"3.97.25.250","node_port":"9701","services":["VALIDATOR"]},"dest":"CfTG6ZPJzXRu9xozL43jzsA8EQXRvkbDHvYw3QzuTQrf"},"metadata":{"from":"XdVFchbXSMC898NjWCymXD"},"type":"0"},"txnMetadata":{"seqNo":2,"txnId":"8a6f8e43b9d9f7020c824e069a521b6682a91d48b3dd31bd71b67caaaf7ada9c"},"ver":"1"}
 {"reqSignature":{},"txn":{"data":{"data":{"alias":"CandyDevOntNode01","blskey":"2Ldaj4ZeYmri7sx9YVGk5x1DRD5NMwbuASQPvEicqC7yQxreCvZYKKrnfi2kiTm26Mss5dm6UUeVFYLPKFwHqXWUnACyMNnHAUBw2HissHNXtLWUVXmPSdAXNgjYVTpPDZ4FuHKWuyDKtghfcb7F6UZSiJwkbVytA4aWoQmFeVrsHSp","blskey_pop":"RGkwsaPoSrERaFTbkLx5CQ8EcthFrNU8LaXsVV4dage4Cqk6eFtqeUj6BRzUaGUAQMFWiRTmfand6AhAt9ghBK6Hi2axf93JEkDChgPUC1Nks9HscMYRxs2AFuXiectW1uHEFW4QtrtWuKyfmBSfyv9rDvJiKCZsiVWaQpecUftdeP","client_ip":"20.200.95.22","client_port":"9702","node_ip":"20.151.226.136","node_port":"9701","services":["VALIDATOR"]},"dest":"DcvgAW7Wuw2aDxakjJGxFnssaXoAgUEWBhRUfNoEeVHx"},"metadata":{"from":"SmQ4y56g72C3ESKVgJTsBL"},"type":"0"},"txnMetadata":{"seqNo":3,"txnId":"3e0f572aa2a2e1c4ebf2ba679438f30f73abce817f3b6af1308c6fcb52823594"},"ver":"1"}
 {"reqSignature":{},"txn":{"data":{"data":{"alias":"CandyDevOntNode02","blskey":"y9Vim7vZ39VkaacrmTcpQ8ytniabf2NSq3RRNAmELx8U93nKN1XhQaAhLJWPBwAjDtWfTP9PeaJUJZ1JNx1b3vmroZzEiPigMViZGBgnSLSiPnNyGCo1WqmSV2XD5QVqFLoRkym7C97vg9p5xTZm2EmBfdDpEc3M5F2tqofDhzFAzi","blskey_pop":"RQrbDZ1uj6QwUqGwzN2LMNw4DvpNmpgsZEYWYVM7e3aWnyiVWijgTV6mp1rjJR83x48P7dN2JYXFer8iTym9iiKvKTKKEMGEGvEUkGE3r3Mxg6hGMKEFkr5aYZMpoqiZ5BWjN52pGjMx2SfEr7FcmcHrwFw8znQvNxn8s1qh9TLCJ3","client_ip":"20.200.77.108","client_port":"9702","node_ip":"20.104.69.119","node_port":"9701","services":["VALIDATOR"]},"dest":"6Ey8JA9YDPzEccbi4dGTaEoPtWK6bvVki6WveWHMUjHb"},"metadata":{"from":"5KwZGBgmVhjAUmc3Ur8wGZ"},"type":"0"},"txnMetadata":{"seqNo":4,"txnId":"5fcd2367ffe5f0f2e63c5807692b0895f3d9db8c10a35a1974f9f507a3f291e3"},"ver":"1"}`
+const ledgerID = 'CANdy-dev'
 const ledgerGenesis = CANdyDev
 
 class AFJAgent {
@@ -38,20 +37,15 @@ class AFJAgent {
     connection_id: string
 }
 
-const config: InitConfig = {
+const agentConfig: InitConfig = {
     label: walletLabel,
     logger: new ConsoleLogger(LogLevel.info),
-    walletConfig: {
-        id: walletLabel,
-        key: walletKey,
-    },
-    indyLedgers: [
-      {
-        genesisTransactions: ledgerGenesis,
-        id: ledgerID,
-        isProduction: false,
-      },
-    ],
+    walletConfig: { id: walletLabel,
+                    key: walletKey},
+    indyLedgers: [ { genesisTransactions: ledgerGenesis,
+                    id: ledgerID,
+                    isProduction: false
+                    }],
     autoAcceptConnections: true,
     autoAcceptProofs: AutoAcceptProof.ContentApproved,
     connectToIndyLedgersOnStartup: true,
@@ -59,7 +53,7 @@ const config: InitConfig = {
 }
 
 const AgentAFJ = new AFJAgent()
-AgentAFJ.agent = new Agent(config, agentDependencies)
+AgentAFJ.agent = new Agent(agentConfig, agentDependencies)
 AgentAFJ.agent.registerOutboundTransport(new HttpOutboundTransport())
 AgentAFJ.agent.registerInboundTransport(new HttpInboundTransport({ port: serviceEndpointPort }))
 AgentAFJ.agent.initialize()
@@ -81,24 +75,21 @@ AgentAFJ.agent.events.on<ConnectionStateChangedEvent>(ConnectionEventTypes.Conne
                           schemaName: "unverified_person",
                           schemaVersion: "0.1.0",
                           issuerDid: "9wVuYYDEDtpZ6CYMqSiWop"})
-                      ]
-          })
+                      ]})
       attributes['attr_2'] = new ProofAttributeInfo({
           name: "family_name",
           restrictions: [ new AttributeFilter({
                           schemaName: "unverified_person",
                           schemaVersion: "0.1.0",
                           issuerDid: "9wVuYYDEDtpZ6CYMqSiWop"})
-                      ]
-          })
+                      ]})
       attributes['attr_3'] = new ProofAttributeInfo({
           name: "region",
           restrictions: [ new AttributeFilter({
                               schemaName: "unverified_person",
                               schemaVersion: "0.1.0",
                               issuerDid: "9wVuYYDEDtpZ6CYMqSiWop"})
-                          ]
-          })
+                          ]})
       console.log("Requesting Proof:", connectionRecord, attributes['attr_1'])
       await AgentAFJ.agent.proofs.requestProof(AgentAFJ.connection_id, {
           requestedAttributes: attributes
@@ -134,7 +125,7 @@ app.get('/', async (req, res) => {
     qrc.toString(invite.invitationUrl, {type:'terminal', 'small': true, 'scale': 1}, function (err: any, url: string) {
         if (err) console.log('qr error')
         console.log(url)
-     })
+    })
     qrc.toString(invite.invitationUrl, {type:'svg'}, function (err: any, url: string) {
         if (err) console.log('qr error')
         inviteQR = url.replace('viewBox=', 'width="600" height="600" viewBox=')
